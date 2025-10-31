3a1f7b9e477835c95a0f12c7f920c3571f2c081b
AnabelleDors <168488755+AnabelleDors@users.noreply.github.com>
2025-10-31 19:25:50 -0300

    Ajustando estruturação das pastas e finalizando terceira entrega

--- the-moviebox-react/src/App.tsx
+++ the-moviebox-react/src/App.tsx
@@ -1,4 +1,4 @@
-import { MovieContent } from "./cases/movies/components/movie.content";
+import { MovieContent } from "./cases/movies/components/movie-content";
 import { Footer } from "./components/layout/footer";
 import { Header } from "./components/layout/header";
 import { NavBar } from "./components/layout/navbar";
