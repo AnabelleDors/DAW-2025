13ca2c69337e3a402c2f30fc1e7ef54551680bc8
AnabelleDors <168488755+AnabelleDors@users.noreply.github.com>
2025-10-31 12:56:50 -0300

    third commit

--- the-moviebox-react/the-moviebox-react/src/components/layout/navbar.tsx
+++ the-moviebox-react/the-moviebox-react/src/components/layout/navbar.tsx
@@ -1,21 +1,29 @@
 const categories = [
-  {label: 'Mais Assistidos', active: true, link: '/Mais-Assistidos'},
-  {label: 'Mais Votados', active: true, link: '/Mais-Votados'},  
-  {label: 'Novidades', active: true, link: '/Novidades'}, 
-  {label: 'Top 10', active: true, link: '/Top 10'},
-  {label: 'Gêneros', active: true, link: '/generos'},   
-];
+    { label: "Mais assistidos", active: true, link: "/mais-assistidos"},
+    { label: "Mais votados", active: true, link: "/mais-votados"},
+    { label: "Novidades", active: true, link: "/novidades"},
+    { label: "Top 10", active: true, link: "/top10"},
+    { label: "Gêneros", active: true, link: "/generos"},
+ ];
 
-
-export function navbar() {
-    return (
-        <nav className="categories container">
-
-            <ol>
-                <li><a href="#" className="active">Mais assistidos</a></li>
-                <li><a href="#">Mais Votados</a> </li>
-                <li><a href="#">Novidades</a> </li>
-                <li><a href="#"> Generos</a></li>
+export function NavBar() {
+    return(
+        <nav className="flex bg-[#1c1c1c] border-b border-b-[#333] max-w-6xl mx-auto">
+            <ol className="flex gap-8 list-none py-6">
+                {categories.map((item)=>(
+                     <li>
+                        <a
+                            href={item.link}
+                            className={`text-sm 
+                                ${item.active 
+                                    ? "text-[#e50914] border-b-2 border-[#e50925] hover:text-[#e50925]" 
+                                    : "text-[#ccc] hover:text-white"
+                                }
+                            `}>
+                            {item.label}
+                        </a>
+                    </li>
+                ))}
             </ol>
         </nav>
     )
