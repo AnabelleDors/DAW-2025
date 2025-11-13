b358875314f4339993866418265cd1190f073b83
AnabelleDors <168488755+AnabelleDors@users.noreply.github.com>
2025-11-13 13:47:31 -0300

    implementacao Tailwind

--- the-moviebox-react/src/App.tsx
+++ the-moviebox-react/src/App.tsx
@@ -1,4 +1,5 @@
 import { MovieContent } from "./cases/movies/components/movie-content";
+import { Highlight } from "./components/layout/highlight";
 import { Footer } from "./components/layout/footer";
 import { Header } from "./components/layout/header";
 import { NavBar } from "./components/layout/navbar";
@@ -9,6 +10,7 @@ function App() {
     <>
       <Header />
       <main>
+        <Highlight/>
         <NavBar/>
         <MovieContent />
       </main>
